55bf49071a3b943ca390b9895f05ebbd758ec09a
Kirill Darenskii <kdarenskii@mail.ru>
2023-05-10 23:17:10 +0300

    Changed url to socket server

--- client/src/hooks/helpers/useSocketConnect.ts
+++ client/src/hooks/helpers/useSocketConnect.ts
@@ -5,11 +5,14 @@ import { useCallback } from "react";
 export const useSocketConnect = () => {
     const { setSocket } = useSocketContext();
 
-    const connect = useCallback((userId: string) => {
-        const socket = io("ws://localhost:9000");
-        socket.emit("addUser", userId);
-        setSocket(socket);
-    }, [setSocket])
+    const connect = useCallback(
+        (userId: string) => {
+            const socket = io(process.env.REACT_APP_SOCKET_SERVER_URL || "http://localhost:9000");
+            socket.emit("addUser", userId);
+            setSocket(socket);
+        },
+        [setSocket]
+    );
 
     return connect;
 };
